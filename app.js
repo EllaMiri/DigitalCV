e285e23d025f48cf1275fa40a24894276b7b3162
EllaMiri <ella.larsson0012@gmail.com>
2021-11-26 13:10:48 +0100

    added scroll into view for all menu tabs

--- app.js
+++ app.js
@@ -7,9 +7,49 @@ function main() {
 function addEventListeners() {
     const menuButton = document.getElementById('burgermenu')
     menuButton.onclick = toggleMenu;
+
+    const home = document.getElementById('home')
+    home.onclick = scrollIntoHome;
+
+    const aboutMe = document.getElementById('about-me')
+    aboutMe.onclick = scrollIntoAboutMe;
+
+    const skills = document.getElementById('skills')
+    skills.onclick = scrollIntoSkills;
+
+    const projects = document.getElementById('projects')
+    projects.onclick = scrollIntoProjects;
+
+    const contacts = document.getElementById('contact')
+    contacts.onclick = scrollIntoContact;
 }
 
 function toggleMenu() {
     const header = document.querySelector('header')
     header.classList.toggle('header-open')
+}
+
+function scrollIntoHome(event) {
+    const homeScroll = document.getElementById('home-scroll')
+    homeScroll.scrollIntoView({ behavior: "smooth"});
+}
+
+function scrollIntoAboutMe(event) {
+    const aboutMeScroll =  document.getElementById('about-me-scroll')
+    aboutMeScroll.scrollIntoView({ behavior: "smooth"});
+}
+
+function scrollIntoSkills(event) {
+    const skillsScroll = document.getElementById('skills-scroll')
+    skillsScroll.scrollIntoView({ behavior: "smooth"});
+}
+
+function scrollIntoProjects(event) {
+    const projectsScroll = document.getElementById('projects-scroll')
+    projectsScroll.scrollIntoView({ behavior: "smooth"});
+}
+
+function scrollIntoContact(event) {
+    const contactScroll = document.getElementById('contact-scroll')
+    contactScroll.scrollIntoView({ behavior: "smooth"});
 }
